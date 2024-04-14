8ebed3d31da4cda79c9dd534ff4541b22fc975c3
Dilan Livera <Dylan_yahampath@yahoo.com>
2024-04-14 16:10:15 +1000

    feat: add orders page

---
+++ Bootstrap.Shop.UI/Orders/OrderController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootstrap.Shop.UI.Orders;
+
+public class OrderController : Controller
+{
+  public IActionResult Index() => View(viewName: "~/Orders/Index.cshtml");
+}
